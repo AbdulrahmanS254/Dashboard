edd82728ff01277804d5a4be483ccb614d1b83b3
Abdulrahman Shawky <hawk18922@gmail.com>
2024-07-10 18:02:17 +0300

    a problem in icons

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,23 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html',
         }),
+        new HtmlWebpackPlugin({
+            filename: 'components/button.html',
+            template: './src/components/button.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'components/textfield.html',
+            template: './src/components/textfield.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'components/card.html',
+            template: './src/components/card.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'components/banner.html',
+            template: './src/components/banner.html',
+        }),
+
         new CleanWebpackPlugin(),
         
         new MiniCssExtractPlugin({
