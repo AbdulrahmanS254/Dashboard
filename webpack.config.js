bb4b3b3079e87abffdc5c9c1f53b06d5d4f6e191
Abdulrahman Shawky <hawk18922@gmail.com>
2024-07-23 18:58:55 +0300

    problem with the console

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 const path = require("path");
 
 module.exports = {
     entry: {
-        "app" : "./src/index.js",
-        "assets/js/banner" : "./src/assets/js/banner.js",
-        "assets/js/tabs" : "./src/assets/js/tabs.js",
-        "assets/js/upload" : "./src/assets/js/upload.js",
-        "assets/js/chart" : "./src/assets/js/chart.js",
+        app: "./src/index.js",
+        "assets/js/banner": "./src/assets/js/banner.js",
+        "assets/js/tabs": "./src/assets/js/tabs.js",
+        "assets/js/upload": "./src/assets/js/upload.js",
+        "assets/js/chart": "./src/assets/js/chart.js",
     },
 
     output: {
@@ -39,7 +40,7 @@ module.exports = {
 
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
                 generator: {
                     filename: "assets/images/[name].[ext]",
                 },
@@ -53,7 +54,7 @@ module.exports = {
                     filename: "assets/fonts/[name].[ext]",
                 },
             },
-            
+
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
@@ -71,78 +72,115 @@ module.exports = {
         new HtmlWebpackPlugin({
             filename: "index.html",
             template: "./src/index.html",
-            chunks: ['app'],
+            chunks: [
+                "app",
+                "assets/js/banner",
+                "assets/js/tabs",
+                "assets/js/upload",
+                "assets/js/chart",
+            ],
         }),
         new HtmlWebpackPlugin({
             filename: "components/button.html",
             template: "./src/components/button.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/textfield.html",
             template: "./src/components/textfield.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/card.html",
             template: "./src/components/card.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/banner.html",
             template: "./src/components/banner.html",
-            chunks: ['app', 'assets/js/banner'],
+            chunks: ["app", "assets/js/banner"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/list.html",
             template: "./src/components/list.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/tabs.html",
             template: "./src/components/tabs.html",
-            chunks: ['app', 'assets/js/tabs'],
+            chunks: ["app", "assets/js/tabs"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/upload.html",
             template: "./src/components/upload.html",
-            chunks: ['app', 'assets/js/upload'],
+            chunks: ["app", "assets/js/upload"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/help.html",
             template: "./src/components/help.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/summary.html",
             template: "./src/components/summary.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/actions.html",
             template: "./src/components/actions.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/sidebar.html",
             template: "./src/components/sidebar.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/table.html",
             template: "./src/components/table.html",
-            chunks: ['app'],
+            chunks: ["app"],
         }),
         new HtmlWebpackPlugin({
             filename: "components/chart.html",
             template: "./src/components/chart.html",
-            chunks: ['app', 'assets/js/chart'],
+            chunks: ["app", "assets/js/chart"],
         }),
 
         new CleanWebpackPlugin(),
 
         new MiniCssExtractPlugin({
             filename: "assets/css/style.css",
+        }),
+
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/help.html"),
+            location: "help",
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/summary.html"),
+            location: "summary",
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/actions.html"),
+            location: "actions",
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/banner.html"),
+            location: "banner",
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/chart.html"),
+            location: "chart",
+            template_filename: ['index.html'],
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: path.join(__dirname, "./src/components/list.html"),
+            location: "list",
+            template_filename: ['index.html'],
         }),
     ],
 
